bcf3b746d93d0e9c25c59d2e30888f46ec622ed9
Brienne Kordis <87245718+briennekordis@users.noreply.github.com>
2024-01-29 17:18:58 -0500

    Update outputUpdate.js

--- outputUpdate.js
+++ outputUpdate.js
@@ -2,6 +2,7 @@ let caseTable = base.getTable('Dev Cases');
 let queryResult = await caseTable.selectRecordsAsync({fields: ["County"]});
 let phrase = '';
 
+// Loop through each of the records, and assign a phrase depending on the county listed. 
 for (let record of queryResult.records) {
    switch(record.getCellValueAsString("County")) {
       case "Baltimore":
@@ -11,7 +12,7 @@ for (let record of queryResult.records) {
          phrase = 'So long and thanks for all the fish'
          break;
       default:
-         phrase = 'A county is needed.'
+         phrase = 'No county; no fish.'
    }
    caseTable.updateRecordAsync(record.id, {'Output': phrase});
 }
