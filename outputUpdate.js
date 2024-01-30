1cd2522d6d414e9b23d660fd5508f5d7be539a3f
Brienne Kordis <87245718+briennekordis@users.noreply.github.com>
2024-01-30 16:07:36 -0500

    cross table update of records

--- outputUpdate.js
+++ outputUpdate.js
@@ -1,18 +1,32 @@
-let caseTable = base.getTable('Dev Cases');
-let queryResult = await caseTable.selectRecordsAsync({fields: ["County"]});
+const caseTable = base.getTable('Dev Cases');
+const assignmentTable = base.getTable('Dev Case Assignments');
+let cases = await caseTable.selectRecordsAsync({fields: ['Id', 'County']});
+let assignments = await assignmentTable.selectRecordsAsync({fields: ['Case Id']});
+let county = '';
 let phrase = '';
 
 // Loop through each of the records, and assign a phrase depending on the county listed. 
-for (let record of queryResult.records) {
-   switch(record.getCellValueAsString("County")) {
-      case "Baltimore":
-         phrase = 'Hello, world!'
-         break;
-      case "Allegany":
-         phrase = 'So long and thanks for all the fish'
-         break;
-      default:
-         phrase = 'No county; no fish.'
-   }
-   caseTable.updateRecordAsync(record.id, {'Output': phrase});
+for (let singleCase of cases.records) {
+  county = singleCase.getCellValueAsString('County');
+  if (county == 'Baltimore') {
+    phrase = 'Hello, world!'
+  } else if (county == 'Allegany') {
+    phrase = 'So long and thanks for all the fish.'
+  } else (
+    phrase = 'No county, no fish.'
+  )
+
+  caseTable.updateRecordAsync(singleCase.id, {'Output': phrase});
+   
+  for (let assignment of assignments.records) {
+    let caseIdFromAssignments = assignment.getCellValueAsString('Case Id');
+    let caseIdFromCases = singleCase.getCellValueAsString('Id');
+    
+    console.log('case id from assignment table:' + caseIdFromAssignments);
+    console.log('case id from case table:' + caseIdFromCases);
+    
+    if (caseIdFromCases == caseIdFromAssignments) {
+       assignmentTable.updateRecordAsync(assignment.id, {'Cross table output': 'DONT PANTIC'});
+    }
+  }
 }
