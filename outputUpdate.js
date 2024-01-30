1cdd552b8d08d3e8de043674a8661d9ef66ed503
Brienne Kordis <87245718+briennekordis@users.noreply.github.com>
2024-01-30 16:17:06 -0500

    change variable name and add comments

--- outputUpdate.js
+++ outputUpdate.js
@@ -1,13 +1,19 @@
 const caseTable = base.getTable('Dev Cases');
 const assignmentTable = base.getTable('Dev Case Assignments');
+const officeTable = base.getTable('Dev Offices');
 let cases = await caseTable.selectRecordsAsync({fields: ['Id', 'County']});
 let assignments = await assignmentTable.selectRecordsAsync({fields: ['Case Id']});
+let offices = await officeTable.selectRecordsAsync({fields: ['Name', 'Planet', 'Counties served']});
 let county = '';
 let phrase = '';
 
-// Loop through each of the records, and assign a phrase depending on the county listed. 
-for (let singleCase of cases.records) {
-  county = singleCase.getCellValueAsString('County');
+// Loop through the case records.
+for (let caseRecord of cases.records) {
+  // Get the value in the 'Id' column.
+  let caseIdFromCases = caseRecord.getCellValueAsString('Id');
+  // Get the value in the 'County' column.
+  county = record.getCellValueAsString('County');
+  // Assign a phrase based on the value of the 'County' column.
   if (county == 'Baltimore') {
     phrase = 'Hello, world!'
   } else if (county == 'Allegany') {
@@ -16,15 +22,19 @@ for (let singleCase of cases.records) {
     phrase = 'No county, no fish.'
   )
 
-  caseTable.updateRecordAsync(singleCase.id, {'Output': phrase});
-   
+  // Update the 'Output' column in the Dev Cases tavke with that phrase.
+  caseTable.updateRecordAsync(caseRecord.id, {'Output': phrase});
+
+  // Loop through the assignment records.
   for (let assignment of assignments.records) {
+    // Get the value in the 'Case Id' column.
     let caseIdFromAssignments = assignment.getCellValueAsString('Case Id');
-    let caseIdFromCases = singleCase.getCellValueAsString('Id');
+
+    // For testing.
+    console.log(`assignmentTable case id: ${caseIdFromAssignments}`);
+    console.log(`caseTable case id:  ${caseIdFromCases}`);
     
-    console.log('case id from assignment table:' + caseIdFromAssignments);
-    console.log('case id from case table:' + caseIdFromCases);
-    
+    // If the Ids from the Cases and Assignments table match, update the 'Cross table output' column.
     if (caseIdFromCases == caseIdFromAssignments) {
        assignmentTable.updateRecordAsync(assignment.id, {'Cross table output': 'DONT PANTIC'});
     }
